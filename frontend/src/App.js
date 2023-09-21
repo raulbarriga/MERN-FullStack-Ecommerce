c3f8a9420b6497a88962fe3f0025cab06be49743
Raul Barriga <barrigaraulaug@gmail.com>
2023-09-21 12:31:28 -0700

    Moved the toast css import from App.js to index.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
